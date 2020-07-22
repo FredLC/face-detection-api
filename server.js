49d14d3496664d6db7a3f55f428e3aa1d92f5686
fredlc <fredlefevre@outlook.com>
2020-07-22 15:12:40 -0400

    Refactored code

--- server.js
+++ server.js
@@ -3,6 +3,11 @@ const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
 
+const register = require('./ controllers/register');
+const signin = require('./ controllers/signin');
+const profile = require('./ controllers/profile');
+const image = require('./ controllers/image');
+
 const db = knex({
   client: 'pg',
   connection: {
@@ -24,96 +29,24 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-const database = {
-  users: [
-    {
-      id: 1,
-      name: 'Joe',
-      email: 'joe@test.com',
-      password: 'football',
-      entries: 0,
-      joined: new Date(),
-    },
-    {
-      id: 2,
-      name: 'Joanna',
-      email: 'joanna@test.com',
-      password: 'password',
-      entries: 0,
-      joined: new Date(),
-    },
-  ],
-};
-
 app.get('/', (req, res) => {
   res.send(database.users);
 });
 
 app.post('/signin', (req, res) => {
-  bcrypt.compare(
-    'joemarambi',
-    '$2a$10$1JQbsTRkVQQWLupEtVR6YObMn8DTuVb5gWrQ3XHJGsg4WvicuT3Li',
-    function (err, res) {
-      console.log('first guess', res);
-    }
-  );
-  bcrypt.compare(
-    'veggies',
-    '$2a$10$1JQbsTRkVQQWLupEtVR6YObMn8DTuVb5gWrQ3XHJGsg4WvicuT3Li',
-    function (err, res) {
-      console.log('second guess', res);
-    }
-  );
-  if (
-    req.body.email === database.users[0].email &&
-    req.body.password === database.users[0].password
-  ) {
-    res.json(database.users[0]);
-  } else {
-    res.status(400).json('error logging in');
-  }
+  signin.handleSignin(req, res, db, bcrypt);
 });
 
 app.post('/register', (req, res) => {
-  const { name, email, password } = req.body;
-  db('users')
-    .returning('*')
-    .insert({
-      email: email,
-      name: name,
-      joined: new Date(),
-    })
-    .then((user) => {
-      res.json(user[0]);
-    })
-    .catch((err) => res.status(400).json('unable to register'));
+  register.handleRegister(req, res, db, bcrypt);
 });
 
 app.get('/profile/:id', (req, res) => {
-  const { id } = req.params;
-  db.select('*')
-    .from('users')
-    .where({ id })
-    .then((user) => {
-      if (user.length) {
-        res.json(user[0]);
-      } else {
-        res.status(400).json('not found');
-      }
-    })
-    .catch((err) => res.status(400).json('Error getting user'));
+  profile.handleGetProfile(req, res, db);
 });
 
 app.put('/image', (req, res) => {
-  const { id } = req.body;
-  db('users')
-    .where('id', '=', id)
-    .increment('entries', 1)
-    .returning('entries')
-    .then((entries) => {
-      res.json(entries[0]);
-    })
-    .catch((err) => res.status(400).json('Unable to get entries'));
+  image.handleUpdateImage(req, res, db);
 });
 
 app.listen(3001, () => {
